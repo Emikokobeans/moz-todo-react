50b3872fda55c1986646f20f0ab2c6621ce46145
Emi <e.wong@hotmail.co.uk>
2021-09-23 10:15:21 +0100

    delete unnecessary files

--- src/App.js
+++ src/App.js
@@ -2,21 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const subject = 'React';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Hello, {subject}!</p>
       </header>
     </div>
   );
