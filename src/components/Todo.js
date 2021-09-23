5705766b4e02c1d75be77cdf79a3cfcef3f813c9
Emi <e.wong@hotmail.co.uk>
2021-09-23 11:49:12 +0100

    add edit function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Todo = (props) => {
-  return (
-    <li className='todo stack-small'>
+  const [isEditing, setEditing] = useState(false);
+
+  const editingTemplate = (
+    <form className='stack-small'>
+      <div className='form-group'>
+        <label className='todo-label' htmlFor={props.id}>
+          New name for {props.name}
+        </label>
+        <input id={props.id} className='todo-text' type='text' />
+      </div>
+      <div className='btn-group'>
+        <button type='button' className='btn todo-cancel'>
+          Cancel
+          <span className='visually-hidden'>renaming {props.name}</span>
+        </button>
+        <button type='submit' className='btn btn__primary todo-edit'>
+          Save
+          <span className='visually-hidden'>new name for {props.name}</span>
+        </button>
+      </div>
+    </form>
+  );
+  const viewTemplate = (
+    <div className='stack-small'>
       <div className='c-cb'>
         <input
           id={props.id}
@@ -10,7 +32,7 @@ const Todo = (props) => {
           defaultChecked={props.completed}
           onChange={() => props.toggleTaskCompleted(props.id)}
         />
-        <label className='todo-label' id={props.id}>
+        <label className='todo-label' htmlFor={props.id}>
           {props.name}
         </label>
       </div>
@@ -26,8 +48,10 @@ const Todo = (props) => {
           Delete <span className='visually-hidden'>{props.name}</span>
         </button>
       </div>
-    </li>
+    </div>
   );
+
+  return <li className='todo'>{isEditing ? editingTemplate : viewTemplate}</li>;
 };
 
 export default Todo;
